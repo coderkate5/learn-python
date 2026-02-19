4f23ed2a111903254155db69ca996e5f30137580
Coder Kate5 <kathy@mixpack.nl>
2026-02-19 20:00:03 +0100

    Add Dog class implementation with sample instances and print statements

---
+++ dog.py
@@ -0,0 +1,12 @@
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+my_dog = Dog("Buddy", 3)
+dog1 = Dog("Max", 5)
+dog2 = Dog("Luna", 2)
+dog3 = Dog("Charlie", 4)
+
+print(my_dog.name)
+print(my_dog.age)
