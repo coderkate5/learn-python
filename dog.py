220d28e3fe771f50034d37ed104f63ce7be67ed7
Coder Kate5 <kathy@mixpack.nl>
2026-02-20 16:51:45 +0100

    Refactor print statements for Dog class instances to include descriptive messages

--- dog.py
+++ dog.py
@@ -8,5 +8,9 @@ dog1 = Dog("Max", 5)
 dog2 = Dog("Luna", 2)
 dog3 = Dog("Charlie", 4)
 
-print(my_dog.name)
-print(my_dog.age)
+print("My dog's name is " + my_dog.name + ". He is " + str(my_dog.age) + " years old.")
+    
+print("Dog 1 is named "+ dog1.name + " and is " + str(dog1.age) + " years old.")
+print("Dog 2 is named "+ dog2.name + " and is " + str(dog2.age) + " years old.")
+print("Dog 3 is named "+ dog3.name + " and is " + str(dog3.age) + " years old.")
+
