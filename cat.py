9361cc9b22bf794278ee6674aca21ae240665b2f
Coder Kate5 <kathy@mixpack.nl>
2026-02-20 16:44:52 +0100

    Add Cat class implementation with sample instances and print statements

---
+++ cat.py
@@ -0,0 +1,12 @@
+class Cat:
+    def __init__(self, name, sex):
+        self.name = name
+        self.sex = sex
+
+my_cat = Cat("Pussycat", "Female")
+cat1 = Cat("Whiskers", "Male")
+
+print(my_cat.name)
+print("My cat is a " + my_cat.sex)
+
+
