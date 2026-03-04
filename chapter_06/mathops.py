c480e43c04a01a7ea06f041f61a7c7b55cc94f40
Coder Kate5 <kathy@mixpack.nl>
2026-03-04 16:43:33 +0100

    Add MathOperations class with static methods add, multiply and is_even

---
+++ chapter_06/mathops.py
@@ -0,0 +1,30 @@
+# Using static methods in python
+class MathOperations:
+    @staticmethod
+    def add(a, b):
+        return a + b
+    
+
+    @staticmethod
+    def multiply(a, b):
+        return a * b
+    
+
+    @staticmethod
+    def is_even(number):
+        if number % 2 == 0:
+            return True
+        else:
+            return False
+        
+
+# use methods
+result1 = MathOperations.add(18, 22)
+result2 = MathOperations.multiply(4, 90)
+result3 = MathOperations.is_even(10)
+result4 = MathOperations.is_even(75)
+
+print("The sum of 18 and 22 = ", result1)
+print("4 times 90 = ", result2)
+print("Is 10 an even number? ", result3)
+print("Is 75 an even number? ", result4)
